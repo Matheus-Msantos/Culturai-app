a4db68587da3518e0e3c7c4ebc6075b0d800f718
Bruno Pontes <bruno_confessor@hotmail.com>
2021-10-13 22:35:38 -0300

    Criado tela sing up

---
+++ app/src/main/java/br/senac/culturai/singUp.kt
@@ -0,0 +1,11 @@
+package br.senac.culturai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class singUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
