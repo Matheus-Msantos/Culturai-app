e06b80b673c8e45fb999dc67efb4a5064f8ccc22
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-10-16 17:21:08 -0300

    Telas Menu/Maps/General Searchc e Category

---
+++ app/src/main/java/br/senac/culturai/Maps.kt
@@ -0,0 +1,11 @@
+package br.senac.culturai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Maps : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+    }
+}
