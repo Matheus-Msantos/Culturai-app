96d8e2f796d16909e8888d1ca6dc7701a3f8d4a9
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-10-18 20:12:09 -0300

    Telas de checkouts

---
+++ app/src/main/java/br/senac/culturai/CheckoutPaymentActivity.kt
@@ -0,0 +1,11 @@
+package br.senac.culturai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutPaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout_payment)
+    }
+}
