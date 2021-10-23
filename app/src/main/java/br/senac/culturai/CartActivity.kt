3e417fa44c7e6709e264095ba676914b025fd89d
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-10-23 17:03:27 -0300

    Ajustando layouts

--- app/src/main/java/br/senac/culturai/CartActivity.kt
+++ app/src/main/java/br/senac/culturai/CartActivity.kt
@@ -2,10 +2,20 @@ package br.senac.culturai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.senac.culturai.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
+
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageCart = binding.imageCart
+        val textCardDate = binding.textCartDate
+        val textCardAddress = binding.textCartAddress
+
     }
 }
